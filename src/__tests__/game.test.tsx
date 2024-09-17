edf5319bbcfe7dd0b64d0c599bbba92885375d5f
Juan Quintero <quinterojuan0246@gmail.com>
2024-09-17 19:58:06 -0300

    SCRUM-24 add test to function fetch games

--- src/__tests__/game.test.tsx
+++ src/__tests__/game.test.tsx
@@ -1,4 +1,4 @@
-import create_game from '@/lib/game';
+import { create_game, fetch_games } from '@/lib/game';
 
 global.fetch = jest.fn();
 
@@ -32,5 +32,38 @@ describe('create_game function', () => {
     const failResult = await create_game({ game_name: 'Test Game', player_name: 'John' });
 
     expect(failResult).toEqual({ status: 'ERROR', message: 'An error occurred while creating the game' });
+  });
+});
+
+describe('fetch_games function', () => {
+  beforeEach(() => {
+    (fetch as jest.Mock).mockClear();
+  });
+  it('should handle both a successful 200 response and a failure', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        status: 'OK', message: 'Game created successfully',
+        games: [
+          {
+            game_id: '1',
+            game_name: 'Test Game 1',
+            num_players: 1,
+          },
+          {
+            game_id: '2',
+            game_name: 'Test Game 2',
+            num_players: 2,
+          },
+        ],
+      }),
+    });
+    const successResult = await fetch_games();
+    expect(successResult.games).toHaveLength(2);
+    expect(successResult.games[0].game_name).toEqual('Test Game 1');
+    expect(successResult.games[1].game_name).toEqual('Test Game 2');
+    expect(successResult.games[0].num_players).toEqual(1);
+    expect(successResult.games[1].num_players).toEqual(2);
+    expect(fetch).toHaveBeenCalledWith('http://localhost:8000/game/get_games');
   });
 });
--- src/lib/game.tsx
+++ src/lib/game.tsx
@@ -1,46 +1,46 @@
-export default async function create_game({game_name, player_name}: {game_name: string, player_name: string}) {
-   if (!game_name || !player_name) {
-     console.error('Error: game_name and player_name must be provided and cannot be empty');
-     return { status: 'ERROR', message: 'Invalid game_name or player_name' };
-   }
- 
-   try {
-     const response = await fetch('http://localhost:8000/create_game', {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify({ game_name, player_name })
-     });
- 
-     if (!response.ok) {
-       throw new Error(`Server responded with status ${response.status}`);
-     }
- 
-     const result = await response.json();
- 
-     return result;
- 
-   } catch (error) {
-     console.error('Failed to create game:', error);
-     return { status: 'ERROR', message: 'An error occurred while creating the game' };
-   }
+export async function create_game({ game_name, player_name }: { game_name: string, player_name: string }) {
+  if (!game_name || !player_name) {
+    console.error('Error: game_name and player_name must be provided and cannot be empty');
+    return { status: 'ERROR', message: 'Invalid game_name or player_name' };
+  }
+
+  try {
+    const response = await fetch('http://localhost:8000/create_game', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ game_name, player_name })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Server responded with status ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    return result;
+
+  } catch (error) {
+    console.error('Failed to create game:', error);
+    return { status: 'ERROR', message: 'An error occurred while creating the game' };
+  }
 }
 
 export async function fetch_games() {
-    try {
-      const response = await fetch('http://localhost:8000/game/get_games');
-  
-      if (!response.ok) {
-        throw new Error(`Server responded with status ${response.status}`);
-      }
-  
-      const result = await response.json();
-  
-      return result;
-  
-    } catch (error) {
-      console.error('Failed to fetch games:', error);
-      return { status: 'ERROR', message: 'An error occurred while fetching the games' };
+  try {
+    const response = await fetch('http://localhost:8000/game/get_games');
+
+    if (!response.ok) {
+      throw new Error(`Server responded with status ${response.status}`);
     }
+
+    const result = await response.json();
+
+    return result;
+
+  } catch (error) {
+    console.error('Failed to fetch games:', error);
+    return { status: 'ERROR', message: 'An error occurred while fetching the games' };
+  }
 }
