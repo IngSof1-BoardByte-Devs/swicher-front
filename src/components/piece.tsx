e90019bf248927686aae91cb1ae2efc1c39e2126
Ramiro Cuellar <cuellar.ramiro564@gmail.com>
2024-09-24 23:38:27 -0300

    SCRUM-32 add Piece component

---
+++ src/components/piece.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+
+export function Piece ({ color } : {color: string}){
+  return (
+    <div
+      className={`w-full h-dvh flex rounded-lg transition-all duration-200 hover:scale-110`}
+      style={{ backgroundColor: color }}
+    />
+  );
+};
+
+export default Piece;
