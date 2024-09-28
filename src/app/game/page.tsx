2f6a7452148a8670b6a2ac5ad1636b4c463b10fc
JuanMazzaforte <juan.mazzaforte@mi.unc.edu.ar>
2024-09-28 16:44:26 -0300

    SCRUM-61 change button and handle leave an end turn

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -8,10 +8,10 @@ import { Winner } from "@components/winner";
 import { Card } from "@components/cards";
 
 const initialJugadores = [
-    { jugador_id: 1, nombre_jugador: "pepe" },
-    { jugador_id: 2, nombre_jugador: "agus" },
-    { jugador_id: 3, nombre_jugador: "juan" },
-    { jugador_id: 4, nombre_jugador: "roberto" },
+    { jugador_id: 1, nombre_jugador: "pepe", abandonado: false },
+    { jugador_id: 2, nombre_jugador: "agus", abandonado: false },
+    { jugador_id: 3, nombre_jugador: "juan", abandonado: false },
+    { jugador_id: 4, nombre_jugador: "roberto", abandonado: false },
 ];
 
 const game_id = 1;
@@ -22,12 +22,29 @@ export function Game() {
     const [jugadores, setJugadores] = useState(initialJugadores);
     const [turnoActual, setTurnoActual] = useState<number>(0);
 
-    if (jugadores.length < 2 || jugadores.length > 4) {
-        console.error("El número de jugadores debe ser entre 2 y 4");
-    }
+    const handleAbandonar = (index: number) => {
+        const jugadoresActualizados = [...jugadores];
+        jugadoresActualizados[index].abandonado = true;
+        setJugadores(jugadoresActualizados);
+    };
 
-    if (jugadores.length === 1) {
-        return <Winner player_name={jugadores[0].nombre_jugador} />;
+    const avanzarTurno = () => {
+        let nuevoTurno = turnoActual;
+        let encontrado = false;
+        while (!encontrado) {
+            nuevoTurno = (nuevoTurno + 1) % jugadores.length;
+            if (!jugadores[nuevoTurno].abandonado) {
+                encontrado = true;
+            }
+        }
+        setTurnoActual(nuevoTurno);
+    };
+
+    const countRestantes = jugadores.filter((jugador) => !jugador.abandonado).length;
+
+    if (countRestantes === 1) {
+        const remainingPlayer = jugadores.find((jugador) => !jugador.abandonado);
+        return <Winner player_name={remainingPlayer?.nombre_jugador || "Unknown Player"} />;
     }
 
     return (
@@ -35,79 +52,115 @@ export function Game() {
             {/* Header: Turno Actual, Nombre de Partida y Color Bloqueado */}
             <div className="col-span-12 grid grid-cols-12 items-center">
                 <div className="col-start-1 col-span-3">
-                    <p className="text-xl">Turno de {jugadores[turnoActual].nombre_jugador}</p>
+                    <p className="text-2xl font-bold">Turno de {jugadores[turnoActual]?.nombre_jugador || "Jugador ausente"}</p>
                 </div>
-                <div className="col-start-5 col-span-4 text-center">
-                    <p className="text-xl">Partida: {game_name}</p>
+                <div className="col-start-5 col-span-3 text-center">
+                    <p className="text-2xl font-bold">Partida: {game_name}</p>
                 </div>
-                <div className="col-start-11 col-span-2 text-right flex mr-5 ">
-                    <p className="text-xl">Color bloqueado: {color_bloqueado}</p>
+                <div className="col-end-13 col-span-3 text-right flex mr-5 ">
+                    <p className="text-2xl font-bold">Blocked: {color_bloqueado}</p>
                 </div>
             </div>
 
             {/* Jugador 0 */}
             <div className="row-start-11 row-span-2 col-start-5 col-span-4 flex flex-col items-center">
-                <p className="text-center">{jugadores[0].nombre_jugador}</p>
-                <div className="grid grid-cols-3 gap-2 mt-2">
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                </div>
+                {jugadores[0]?.abandonado ? (
+                    <p className="text-center block font-bold text-red-500">Jugador abandonó</p>
+                ) : (
+                    <>
+                        <p className="text-center block font-bold">{jugadores[0].nombre_jugador}</p>
+                        <div className="grid grid-cols-6 gap-2 mt-2">
+                            {Array(3).fill(null).map((_, index) => (
+                                <button key={index} className="w-16 h-16">
+                                    <Card link="c0" />
+                                </button>
+                            ))}
+                            {Array(3).fill(null).map((_, index) => (
+                                <button key={index} className=" ml-3 w-16 h-16">
+                                    <Card link="c1" />
+                                </button>
+                            ))}
+                        </div>
+                    </>
+                )}
             </div>
 
-            {/* Jugador 1*/}
-            <div className="row-start-2 row-span-2 col-start-5 col-span-4 flex flex-col items-center">
-                <p className="text-center">{jugadores[1].nombre_jugador}</p>
-                <div className="grid grid-cols-3 gap-2 mt-2">
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                </div>
+            {/* Jugador 1 */}
+            <div className="row-start-2 row-span-2 row-end-3 col-start-5 col-span-4 flex flex-col items-center">
+                {jugadores[1]?.abandonado ? (
+                    <p className="text-center block font-bold text-red-500">Jugador abandonó</p>
+                ) : (
+                    <>
+                        <p className="text-center block font-bold">{jugadores[1].nombre_jugador}</p>
+                        <div className="grid grid-cols-6 gap-2 mt-2">
+                            {Array(3).fill(null).map((_, index) => (
+                                <button key={index} className="w-16 h-16">
+                                    <Card link="c0" />
+                                </button>
+                            ))}
+                            {Array(3).fill(null).map((_, index) => (
+                                <button key={index} className=" ml-3 w-16 h-16">
+                                    <Card link="c1" />
+                                </button>
+                            ))}
+                        </div>
+                    </>
+                )}
             </div>
 
             {/* Jugador 2 */}
-            <div className="row-start-5 row-span-2 col-start-1 col-span-3 flex flex-col items-center">
-                <p className="text-center">{jugadores[2].nombre_jugador}</p>
-                <div className="grid grid-cols-3 gap-2 mt-2">
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
+            {jugadores.length >= 3 && (
+                <div className="row-start-5 row-span-2 col-start-1 col-span-3 flex flex-col items-center">
+                    {jugadores[2]?.abandonado ? (
+                        <p className="text-center block font-bold text-red-500">Jugador abandonó</p>
+                    ) : (
+                        <>
+                            <p className="text-center block font-bold">{jugadores[2].nombre_jugador}</p>
+                            <div className="grid grid-cols-3 gap-2 mt-2">
+                                {Array(3).fill(null).map((_, index) => (
+                                    <button key={index} className="w-16 h-16">
+                                        <Card link="c0" />
+                                    </button>
+                                ))}
+                            </div>
+                            <div className="grid grid-cols-3 gap-2 mt-2">
+                                {Array(3).fill(null).map((_, index) => (
+                                    <button key={index} className="w-16 h-16">
+                                        <Card link="c1" />
+                                    </button>
+                                ))}
+                            </div>
+                        </>
+                    )}
                 </div>
-            </div>
+            )}
 
             {/* Jugador 3 */}
-            <div className="row-start-5 row-span-2 col-start-10 col-span-3 flex flex-col items-center">
-                <p className="text-center">{jugadores[3].nombre_jugador}</p>
-                <div className="grid grid-cols-3 gap-2 mt-2">
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
-                    <div className="w-16 h-16">
-                        <Card link="c0" />
-                    </div>
+            {jugadores.length === 4 && (
+                <div className="row-start-5 row-span-2 col-start-10 col-span-3 flex flex-col items-center">
+                    {jugadores[3]?.abandonado ? (
+                        <p className="text-center block font-bold text-red-500">Jugador abandonó</p>
+                    ) : (
+                        <>
+                            <p className="text-center block font-bold">{jugadores[3].nombre_jugador}</p>
+                            <div className="grid grid-cols-3 gap-2 mt-2">
+                                {Array(3).fill(null).map((_, index) => (
+                                    <button key={index} className="w-16 h-16">
+                                        <Card link="c0" />
+                                    </button>
+                                ))}
+                            </div>
+                            <div className="grid grid-cols-3 gap-2 mt-2">
+                                {Array(3).fill(null).map((_, index) => (
+                                    <button key={index} className="w-16 h-16">
+                                        <Card link="c1" />
+                                    </button>
+                                ))}
+                            </div>
+                        </>
+                    )}
                 </div>
-            </div>
+            )}
 
             {/* Tablero de juego */}
             <div className="row-start-4 row-span-6 col-start-4 col-span-6 flex items-center justify-center">
@@ -115,13 +168,13 @@ export function Game() {
             </div>
 
             {/* Botones de acción */}
-            <div className="row-start-12 col-start-9 col-end-11 flex justify-end">
+            <div className="row-start-12 col-start-1 col-end-3 flex justify-start">
                 <button
                     onClick={() => {
                         EndTurn(jugadores[turnoActual].jugador_id);
-                        setTurnoActual(turnoActual === jugadores.length - 1 ? 0 : turnoActual + 1);
+                        avanzarTurno(); 
                     }}
-                    className="border-2 text-white p-2 rounded bg-gray-900 hover:bg-gray-700 transition w-full"
+                    className="border-2 text-white p-2 rounded bg-slate-700 hover:hover:bg-gray-700/95 transition w-full"
                 >
                     Pasar turno
                 </button>
@@ -130,10 +183,10 @@ export function Game() {
                 <button
                     onClick={() => {
                         LeaveGame({ player_id: jugadores[turnoActual].jugador_id, game_id });
-                        setJugadores(jugadores.filter((_, index) => index !== turnoActual));
-                        setTurnoActual(turnoActual === jugadores.length - 1 ? turnoActual - 1 : turnoActual);
+                        handleAbandonar(turnoActual);
+                        avanzarTurno(); 
                     }}
-                    className="border-2 text-white p-2 rounded bg-gray-900 hover:bg-gray-700 transition w-full"
+                    className="border-2 text-white p-2 rounded bg-slate-700 hover:hover:bg-gray-700/95 transition w-full"
                 >
                     Abandonar partida
                 </button>
