489a370c3661bc034f415fd94462f70f04d0d6ea
Ramiro Cuellar <cuellar.ramiro564@gmail.com>
2024-09-28 18:40:47 -0300

    SCRUM-60 add lobby page component

---
+++ src/app/lobby/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { List } from "@/components/list";
+import { start_game } from "@/lib/game";
+import { useCookies } from "react-cookie";
+import clsx from "clsx";
+
+export default function LobbyPage() {
+  const [error, setError] = useState<string>("");
+  const [cookie, setCookie] = useCookies(["player_id", "game_id"]);
+  const mockPlayers = ["Player1", "Player2", "Player3", "Player4"];
+  useEffect(() => {
+    fetch_players().then((data) => {});
+  });
+
+  const handleStartGame = async () => {
+    const result = await start_game({
+      game_id: gameId,
+      player_id: playerId,
+    });
+
+    if (result.status === "ERROR") {
+      setError(result.message);
+    }
+    console.log(result.message);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold uppercase">Nombre de partida</h1>
+        <h1 className="text-lg font-semibold">LOBBY</h1>
+      </div>
+      <div className="w-full border overflow-auto">
+        <div className="flex-col flex divide-y-2">
+          {mockPlayers.map((player_id) => {
+            return (
+              <button
+                key={player_id}
+                className="p-4 hover:bg-gray-200 dark:hover:bg-gray-600 cursor-default"
+              >
+                <div className="flex justify-center">
+                  <div>{player_id}</div>
+                </div>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex justify-center mt-12 w-full">
+        <button
+          onClick={handleStartGame}
+          type="button"
+          className="border dark:rounded-none shadow rounded p-2 dark:bg-inherit dark:hover:bg-gray-600 bg-slate-700 hover:hover:bg-gray-700/95 text-white capitalize"
+        >
+          Comenzar Partida
+        </button>
+        {error && (
+          <p className="text-red-500 max-w-full text-sm text-center mt-2">
+            {error}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
