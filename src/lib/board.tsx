07cddc03b8df9aebcbeaee8d4efd94373ad936aa
JuanMazzaforte <juan.mazzaforte@mi.unc.edu.ar>
2024-09-28 22:46:31 -0300

    SCRUM-85 fix using the api

--- src/lib/board.tsx
+++ src/lib/board.tsx
@@ -21,3 +21,31 @@ export async function fetch_board({ game_id }: { game_id: number }) {
         return { status: 'ERROR', message: 'An error occurred while fetching the board' };
     }
 }
+export async function EndTurn(player_id: number) {
+
+    if (!player_id) {
+        console.error("Error: el player_id must be filed")
+        return {status: "ERROR", message:"invalid player id"}
+    };
+
+    try {
+        const response = await fetch("http://localhost:8000/end-turn", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ "player_id": player_id }),
+          });
+          if (!response.ok) {
+            throw new Error (`Server responded with ${response.status}`);         
+          }
+          const result = await response.json();
+          return result;
+    } catch (error) {
+     console.error("Faild to end turn:", error);
+     return{
+        status: "ERROR",
+        message: "An error occurred while ending the turn"
+     }
+    }
+}
--- src/lib/game.tsx
+++ src/lib/game.tsx
@@ -138,3 +138,35 @@ export async function fetch_players({player_id}: { player_id: number}) {
     };
   }
 }
+export async function LeaveGame({player_id, game_id}: {player_id: number, game_id: number}) {
+
+  if (!player_id) {
+      console.error("Error: el player_id must be filed")
+      return {status: "ERROR", message:"invalid player id"}
+  };
+  if (!game_id) {
+      console.error("Error: el game_id must be filed")
+      return {status: "ERROR", message:"invalid game id"}
+  };
+
+  try {
+      const response = await fetch("http://localhost:8000/game/leave_game", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ "player_id": player_id, "game_id": game_id }),
+        });
+        if (!response.ok) {
+          throw new Error (`Server responded with ${response.status}`);         
+        }
+        const result = await response.json();
+        return result;
+  } catch (error) {
+   console.error("Faild to leave the game:", error);
+   return{
+      status: "ERROR",
+      message: "An error occurred while living the game"
+   }
+  }
+}
