c27d8db4678fde8158be6a5c98ab89cdb7339de2
Juan Quintero <quinterojuan0246@gmail.com>
2024-10-07 11:20:24 -0300

    ING-112 add function to patch card

--- src/lib/card.tsx
+++ src/lib/card.tsx
@@ -1,3 +1,17 @@
+export interface movement_card {
+    id: number;
+    id_player: number;
+    type: number;
+}
+
+export interface figure_card {
+    id: number;
+    id_player: number;
+    type: number;
+    discarded: boolean;
+    blocked: boolean;
+}
+
 export async function fetch_figure_cards({ id_game }: { id_game: number }) {
     try {
         const response = await fetch(`http://localhost:8000/games/${id_game}/figure-cards`);
@@ -35,4 +49,33 @@ export async function fetch_movement_cards({ id_player }: { id_player: number })
             message: "An error occurred while getting the movement cards"
         }
     }
+}
+
+export async function use_movement_cards(
+    { id_player, id_card, index1, index2 }: 
+    { id_player: number, id_card: number, index1:number, index2:number }) {
+    try {
+        const response = await fetch(`http://localhost:8000/movement-cards/${id_card}/`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id_player,
+                index1,
+                index2,
+            })
+        });
+        
+        if (response.status === 200) {
+            return 
+        }
+    } catch (error) {
+        console.error("Failed to use movement cards:", error);
+        return {
+            status: "ERROR",
+            message: "An error occurred while using the movement cards"
+        }
+    }
+
 }
